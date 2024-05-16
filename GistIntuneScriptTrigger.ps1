2f21b0d9bfbdc38a8242c9971de6f317d8f3ebc4
MrWyss-MSFT <mawyss@microsoft.com>
2024-05-16 14:40:07 +0200

    fix merge

--- GistIntuneScriptTrigger.ps1
+++ GistIntuneScriptTrigger.ps1
@@ -4,7 +4,7 @@ $Version = "v0.1.2"
 $Title = @"
 GIST - Gist Intune Script Trigger $Version ($Branch) by https://x.com/MrWyss 
 Source: https://github.com/MrWyss-MSFT/gist
-`u{1F195}: Replaced DO-Monitor with itwaman's version
+`u{1F195}: Replaced DO-Monitor with itwaman's version 
 "@
 
 $GistCatalog = @(
